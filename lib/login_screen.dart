274f19b81f181f4c10110536f4a524b19dbcfee4
Mahmoud Gabr <mahmoudgabr1121@gmail.com>
2024-10-21 17:31:19 +0300

    create native splash screen

---
+++ lib/login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static String id = '/';
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SizedBox(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pic_point/login_screen.dart';
 
 void main() {
   runApp(const PicPoint());
@@ -9,7 +10,11 @@ class PicPoint extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
+      routes: {
+        LoginScreen.id:(context) => const LoginScreen(),
+      }
+      
     );
   }
 }
