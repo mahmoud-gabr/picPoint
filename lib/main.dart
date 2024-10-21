11f33669fc47bd8ae5b78c2f2749a21563b51f03
Mahmoud Gabr <mahmoudgabr1121@gmail.com>
2024-10-21 16:44:49 +0300

    create the project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PicPoint());
+}
+
+class PicPoint extends StatelessWidget {
+  const PicPoint({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+    );
+  }
+}
